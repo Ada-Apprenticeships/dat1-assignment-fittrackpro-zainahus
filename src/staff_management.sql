a702ac22af17811a702e1a0925fc70d1c3c87697
zainahus <zaina.hussain@ada.ac.uk>
2025-02-18 16:58:20 +0000

    sve

--- src/staff_management.sql
+++ src/staff_management.sql
@@ -9,5 +9,14 @@
 -- 1. List all staff members by role
 -- TODO: Write a query to list all staff members by role
 
+SELECT position, staff_id, first_name, last_name
+FROM Staff
+ORDER BY position, last_name, first_name;
 -- 2. Find trainers with one or more personal training session in the next 30 days
 -- TODO: Write a query to find trainers with one or more personal training session in the next 30 days
+
+SELECT DISTINCT s.staff_id, s.first_name, s.last_name, s.email, s.phone_number
+FROM Staff s
+JOIN TrainingSessions ts ON s.staff_id = ts.trainer_id
+WHERE s.position = 'Trainer'
+AND ts.session_date BETWEEN CURRENT_DATE AND DATE_ADD(CURRENT_DATE, INTERVAL 30 DAY);
